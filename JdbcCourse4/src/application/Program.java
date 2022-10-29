3ed4ae4d41d7bc5b3b63c1fdfe1a562eabc838d1
Luis Filipe de Sousa <luisfilipesousa2001@gmail.com>
2022-10-29 09:55:26 -0300

    Exercicios jdbc

---
+++ JdbcCourse4/src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"DELLET FROM department"
+					+ "WHERE"
+					+ "(Id = ?)");
+			st.setInt(1, 5);
+		}
+		catch(SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+	}
+}
